fa45a25e5eb71fc7f816ace58e7f067c4abd4209
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-04 12:42:03 -0300

    agregar-ingreso-cantidad-numeros
    Se creó una pantalla principal que permite al usuario definir la cantidad de números  que va a ingresar

---
+++ ejercicio-numeros.js
@@ -0,0 +1,24 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+const $botonSiguiente = document.querySelector("#siguiente");
+
+$botonSiguiente.onclick = function () {
+  const cantidadIngresada = Number(
+    document.querySelector("#ingreso-cantidad-numeros").value
+  );
+
+  ocultarIngresoPrincipal ();
+
+  return false;
+};
+
+
+function ocultarIngresoPrincipal () {
+    document.querySelector('#ingreso-cantidad-numeros').className = "oculto";
+}
