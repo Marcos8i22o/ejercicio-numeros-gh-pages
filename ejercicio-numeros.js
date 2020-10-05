1136c53fee2bcaf01dbff26f5d2db5052789d913
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-05 08:50:56 -0300

    agrega-funciones
    Se agregaron las funciones:
    * mostrarInputsNumeros: que muestra los campos a completar para los números.
    *guardarNumerosIngresados:  para guardar los números ingresados en un array.
    * mostrarBotonGuardar

--- ejercicio-numeros.js
+++ ejercicio-numeros.js
@@ -7,18 +7,53 @@
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
 const $botonSiguiente = document.querySelector("#siguiente");
+const $botonGuardar = document.querySelector("#guardar");
 
 $botonSiguiente.onclick = function () {
   const cantidadIngresada = Number(
     document.querySelector("#ingreso-cantidad-numeros").value
   );
 
-  ocultarIngresoPrincipal ();
+  ocultarIngresoPrincipal();
+  mostrarInputsNumeros(cantidadIngresada);
 
   return false;
 };
 
+$botonGuardar.onclick = function () {
+  guardarNumerosIngresados();
+};
 
-function ocultarIngresoPrincipal () {
-    document.querySelector('#ingreso-principal').className = "oculto";
+function ocultarIngresoPrincipal() {
+  document.querySelector("#ingreso-principal").className = "oculto";
+}
+
+function mostrarInputsNumeros(cantidadIngresada) {
+  const $listaNumeros = document.querySelector("#lista-numeros");
+
+  for (let i = 0; i < cantidadIngresada; i++) {
+    mostrarBotonGuardar();
+
+    const $numeroAIngresar = document.createElement("li");
+    const $input = document.createElement("input");
+
+    $input.type = "number";
+    $input.className = "numeros-ingresados";
+
+    $numeroAIngresar.appendChild($input);
+
+    $listaNumeros.appendChild($numeroAIngresar);
+  }
+}
+
+function guardarNumerosIngresados() {
+  const numerosIngresados = document.querySelectorAll(".numeros-ingresados");
+  const arrayNumerosIngresados = [];
+  for (let i = 0; i < numerosIngresados.length; i++) {
+    arrayNumerosIngresados.push(Number(numerosIngresados[i].value));
+  }
+}
+
+function mostrarBotonGuardar() {
+  $botonGuardar.className = "";
 }
